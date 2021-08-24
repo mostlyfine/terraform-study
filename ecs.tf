07adafbe60af350533e55bcd1e226cc3dc28c8b5
Seiji Sawayanagi <mostlyfine@gmail.com>
2021-08-24 17:05:02 +0900

    Add ECS.

---
+++ ecs.tf
@@ -0,0 +1,56 @@
+resource "aws_ecs_cluster" "example" {
+  name = "example"
+}
+
+resource "aws_ecs_task_definition" "example" {
+  family                   = "example"
+  cpu                      = 256
+  memory                   = 512
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  container_definitions    = file("./container_definitions.json")
+}
+
+resource "aws_ecs_service" "example" {
+  name                              = "example"
+  cluster                           = aws_ecs_cluster.example.arn
+  task_definition                   = aws_ecs_task_definition.example.arn
+  desired_count                     = 2
+  launch_type                       = "FARGATE"
+  platform_version                  = "1.4.0"
+  health_check_grace_period_seconds = 60
+
+  network_configuration {
+    assign_public_ip = false
+    security_groups  = [module.nginx_sg.security_group_id]
+    subnets = [
+      aws_subnet.private_0.id,
+      aws_subnet.private_1.id,
+    ]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.example.arn
+    container_name   = "example"
+    container_port   = 80
+  }
+
+  lifecycle {
+    ignore_changes = [
+      desired_count,
+      task_definition,
+    ]
+  }
+
+  depends_on = [
+    aws_lb.example
+  ]
+}
+
+module "nginx_sg" {
+  source      = "./security_group"
+  name        = "nginx_sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 80
+  cidr_blocks = [aws_vpc.example.cidr_block]
+}
