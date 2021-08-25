02e40ccc4af6ef6fab9ddc906553cc91add651c0
Seiji Sawayanagi <mostlyfine@gmail.com>
2021-08-25 14:37:52 +0900

    Deployable via Github Actions.

---
+++ ecr.tf
@@ -0,0 +1,26 @@
+resource "aws_ecr_repository" "example" {
+  name = "example-repository"
+}
+
+resource "aws_ecr_lifecycle_policy" "example" {
+  repository = aws_ecr_repository.example.name
+  policy     = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Keep last 10 release tagged images",
+        "selection": {
+          "tagStatus": "tagged",
+          "tagPrefixList": ["release"],
+          "countType": "imageCountMoreThan",
+          "countNumber": 10
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+EOF
+}
